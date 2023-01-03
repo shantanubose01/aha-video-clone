3f093e043a5e667395dd031598b8f511953dac57
Adil-khan-007 <adilk9030@gmail.com>
2023-01-03 20:16:39 +0530

    initial commit

---
+++ src/components/IndividualMovie.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IndividualMovie(props) {
+    return (
+        <div>
+            <h1>Individual Component</h1>
+        </div>
+    );
+}
+
+export default IndividualMovie;
