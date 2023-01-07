5fdadf6b20339f468ca25402c8f3e73b52515069
faran <faranmd1@gmail.com>
2023-01-08 01:13:02 +0530

    add ;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import Home from './pages/Home/Home';
-import AllRoutes from './routes/AllRoutes'
-
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-    
       <AllRoutes />
     </div>
   );
