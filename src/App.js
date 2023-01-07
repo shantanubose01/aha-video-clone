df3d0acc1ce3092bdd76e9ee2986b2849bbc2e0f
faran <faranmd1@gmail.com>
2023-01-08 01:18:49 +0530

    add Allroutes to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import Home from './pages/Home/Home';
-
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-    
-      <Home />
+      <AllRoutes />
     </div>
   );
 }
