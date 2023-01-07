5a87f8ea887b3ebd25dab23cf8c9deea009596e9
faran <faranmd1@gmail.com>
2023-01-07 20:32:35 +0530

    add browserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 // import './App.css';
 import Home from './pages/Home/Home';
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <AllRoutes />
     </div>
   );
 }
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Search from "../pages/Search/Search";
 
 const AllRoutes = () => {
     return ( 
         <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/movies" element={<Movies />}></Route>
-            <Route path="/shows" element={<Shows/>}></Route>
-            <Route path="/myaha" element={<Myaha/>}></Route>
+            {/* <Route path="/movies" element={<Movies />}></Route> */}
+            {/* <Route path="/shows" element={<Shows/>}></Route> */}
+            {/* <Route path="/myaha" element={<Myaha/>}></Route> */}
             <Route path="/search" element={<Search/>}></Route>
-            <Route path="/movies/:id" element={<MoviePage/>}></Route>
-            <Route path="/shows/:id" element={<ShowPage/>}></Route>
-            <Route path="/player" element={<Player/>}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
+            {/* <Route path="/movies/:id" element={<MoviePage/>}></Route> */}
+            {/* <Route path="/shows/:id" element={<ShowPage/>}></Route> */}
+            {/* <Route path="/player" element={<Player/>}></Route> */}
+            {/* <Route path="*" element={<NotFound/>}></Route> */}
 
         </Routes>
      );
