04911ab985737e42fff6a1674e9a0a128320bdd4
faran <faranmd1@gmail.com>
2023-01-08 08:06:44 +0530

    make changes in show, player

--- src/components/Cards/HorizontalCard.jsx
+++ src/components/Cards/HorizontalCard.jsx
@@ -1,9 +1,19 @@
 import style from './HorizontalCard.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function HorizontalCard(props) {
-    const {image, title} = props;
+    const {image, title, id} = props;
+
+    const navigate = useNavigate();
+
+    const handleClick = (e) =>{
+        // console.log(id)
+        localStorage.setItem("tmdbID", id.toString());
+        navigate("/player");
+    }
+
     return ( 
-        <div className={style.horizontalcard}>
+        <div className={style.horizontalcard} onClick={handleClick}>
             <div className={style.imgContainer2}>
                 <img className={style.img2} src={image} alt="title"/>
             </div>
--- src/pages/Shows/ListShowsWide.jsx
+++ src/pages/Shows/ListShowsWide.jsx
@@ -50,21 +50,22 @@ function ListShowsWide(props) {
       </div>
       <div className={style.horizontalList} id={props.id}>
         {data?.map((e, i) => {
-            let imgurl;
-            if(e.backdrop_path){
-                imgurl = `https://image.tmdb.org/t/p/w1280${e.backdrop_path}`;
-            }
-            else if(e.poster_path ){
-                imgurl = `https://image.tmdb.org/t/p/w1280${e.poster_path}`;
-            }
-            else if(e.still_path){
-                imgurl = `https://image.tmdb.org/t/p/w1280${e.still_path}`;
-            }
-            else{
-                imgurl = `https://image-resizer-cloud-api.akamaized.net/image/A5195311-35E3-46F0-BA14-58F1B96AD6ED/0-16x9.jpg?width=336`;
-            }
+          let imgurl;
+          if (e.backdrop_path) {
+            imgurl = `https://image.tmdb.org/t/p/w1280${e.backdrop_path}`;
+          } else if (e.poster_path) {
+            imgurl = `https://image.tmdb.org/t/p/w1280${e.poster_path}`;
+          } else if (e.still_path) {
+            imgurl = `https://image.tmdb.org/t/p/w1280${e.still_path}`;
+          } else {
+            imgurl = `https://image-resizer-cloud-api.akamaized.net/image/A5195311-35E3-46F0-BA14-58F1B96AD6ED/0-16x9.jpg?width=336`;
+          }
           return (
-            <HorizontalCard image={imgurl} title={e.title || e.name} key={i} />
+            <HorizontalCard 
+            image={imgurl} 
+            title={e.title || e.name} 
+            key={i}
+            id={e.id} />
           );
         })}
       </div>
