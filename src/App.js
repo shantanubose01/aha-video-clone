ba94bc21e27be83c1cd41bcd740785af222edaef
priyaa74 <priyavarshney070497@gmail.com>
2023-01-08 21:49:56 +0530

    resolving issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AllRoutes from "./routes/AllRoutes";
 
-import './App.css';
 function App() {
   return (
     <div className="App">
+      <AllRoutes />
     </div>
   );
 }
